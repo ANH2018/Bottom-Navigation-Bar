41e93c24e6907ad3c649a1081ebf14b556760ce2
QUOCANH <youtblife@gmail.com>
2024-04-19 00:00:08 +0700

    them backgroud  image

--- lib/main_screen/custemhome_screen.dart
+++ lib/main_screen/custemhome_screen.dart
@@ -2,62 +2,60 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class customhomescreen extends StatefulWidget {
-  const customhomescreen({super.key});
+  const customhomescreen({Key? key}) : super(key: key);
 
   @override
   State<customhomescreen> createState() => _customhomescreenState();
 }
 
 class _customhomescreenState extends State<customhomescreen> {
-  @override
-  final List <Widget> tabs =const[
-    Center(child: Text("Homesreen"),),
-    Center(child: Text("catelogysreen"),),
-    Center(child: Text("storesreen"),),
-    Center(child: Text("cartsreen"),),
-    Center(child: Text("profilesreen"),),
-
+  final List<Widget> tabs = const [
+    Center(child: Image(image: AssetImage('asset/test_theme.jpg'))), // Thêm hình ảnh vào tab Home
+    Center(child: Text("catelogysreen")),
+    Center(child: Text("storesreen")),
+    Center(child: Text("cartsreen")),
+    Center(child: Text("profilesreen")),
   ];
-  int _selecindex=0;
+  int _selectedIndex = 0;
 
+  @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-         backgroundColor:Colors.blue,
-         body: tabs[_selecindex],
-         bottomNavigationBar:BottomNavigationBar( 
-          backgroundColor:Colors.blue,
-          type: BottomNavigationBarType.fixed, // canh chinh
-          unselectedItemColor:Colors.red,
-          selectedItemColor: Colors.black,
-          currentIndex: _selecindex,
-          items:const [
-              BottomNavigationBarItem(
-                icon: Icon(Icons.home),
-                label: 'Home ',),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.search),
-                label: 'catelogy',),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.shop),
-                label: 'store ',),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.shopping_cart),
-                label: 'cart ',), 
-              BottomNavigationBarItem(
-                icon: Icon(Icons.person),
-                label: 'profile ',),
-
-         ],
-         
-         onTap: (index)
-         {
+    return Scaffold(
+      body: tabs[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.blue,
+        type: BottomNavigationBarType.fixed,
+        unselectedItemColor: Colors.red,
+        selectedItemColor: Colors.black,
+        currentIndex: _selectedIndex,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Catelogy',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shop),
+            label: 'Store',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: 'Cart',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        onTap: (index) {
           setState(() {
-            _selecindex=index;
+            _selectedIndex = index;
           });
-         }
-         
-         ),
-
+        },
+      ),
     );
   }
 }
