0a98cfb2569ed58cd6ee36adc0f7037a76c46e3d
QUOCANH <youtblife@gmail.com>
2024-04-21 22:47:57 +0700

    Them DTC QUAN LY APPBAR VÀ TABBAR

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 class homescreen extends StatefulWidget {
@@ -10,15 +11,31 @@ class homescreen extends StatefulWidget {
 class _homescreenState extends State<homescreen> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return  DefaultTabController(
       
-        appBar: AppBar(
-            elevation: 0,
-            backgroundColor: Colors.white,
-            title: const CupertinoSearchTextField(),
-        ),
+      
+              length:3 , 
+              child: Scaffold(
+              
+                  appBar: AppBar(
+                      elevation: 0,
+                      backgroundColor: Colors.white,
+                      title: const CupertinoSearchTextField(),
+                      bottom: TabBar(
+                        tabs: [
 
-         
+                          Tab(child: Text('men screen',style:TextStyle(color: Colors.grey.shade600) ,),),
+
+                          Tab(child: Text('women screen',style:TextStyle(color: Colors.grey.shade600) ,),),
+
+                          Tab(child: Text('shoes screen',style:TextStyle(color: Colors.grey.shade600) ,),),
+
+                        ],
+                        ),
+                  ),
+              
+           
+                         ),
     );
   }
 }
